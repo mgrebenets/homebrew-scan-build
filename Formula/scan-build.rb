3166ffe62f49790245f9142c89d3f19399a577b5
Maksym Grebenets <maksym.grebenets@cba.com.au>
2017-01-20 19:31:47 +1100

    Release 279

--- Formula/scan-build.rb
+++ Formula/scan-build.rb
@@ -1,8 +1,8 @@
 class ScanBuild < Formula
   desc "Clang Static Analyzer"
   homepage "http://clang-analyzer.llvm.org/scan-build.html"
-  url "http://clang-analyzer.llvm.org/downloads/checker-278.tar.bz2"
-  sha256 "f7180d530eceafc006f30461895ac23033b3b432c41363a185f2234535a09860"
+  url "https://clang-analyzer.llvm.org/downloads/checker-279.tar.bz2"
+  sha256 "f583b58d207e14ce6fc64a623c11d0d8001ab69ffb444b73ba638436a245c784"
 
   def install
     prefix.install Dir["*"]
